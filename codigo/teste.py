84f0381030252ecc9b4fbb4a0d9dea63a3441c97
Alberto Ramos <jalbertosr12@gmail.com>
2025-06-07 00:02:29 +0100

    final ornanization

--- codigo/teste.py
+++
@@ -1,7 +0,0 @@
-import torch
-print("Versão:", torch.__version__)
-print("CUDA disponível:", torch.cuda.is_available())
-print("GPU usada:", torch.cuda.get_device_name(0))
-print("Número de GPUs disponíveis:", torch.cuda.device_count())
-print("Número de núcleos CUDA:", torch.cuda.get_device_capability(0))
-print("Memória total da GPU:", torch.cuda.get_device_properties(0).total_memory / (1024 ** 3), "GB")
